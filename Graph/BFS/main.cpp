99c2d0bea168c9ab634b4a7de91c9c07dae54d37
Anish Gupta <anishgupta675@gmail.com>
2023-03-19 21:27:23 +0530

    BFS

---
+++ Graph/BFS/main.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        bool *vis = (bool *)malloc(V * sizeof(bool));
+        vector<int> res;
+        queue<int> q;
+        vis[0] = 1;
+        q.push(0);
+        while(!q.empty()) {
+            int t = q.front();
+            q.pop();
+            res.push_back(t);
+            for(int i : adj[t]) {
+                if(vis[i]) continue;
+                vis[i] = 1;
+                q.push(i);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
