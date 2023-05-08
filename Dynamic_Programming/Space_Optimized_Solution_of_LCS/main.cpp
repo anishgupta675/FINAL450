2e5c75399eefb891592da75e692c4a4e4b95be22
Anish Gupta <anishgupta675@gmail.com>
2023-05-08 23:57:35 +0530

    day 66 commit

---
+++ Dynamic_Programming/Space_Optimized_Solution_of_LCS/main.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        // your code here
+        vector<vector<int>> dp(s1.length() + 1, vector<int>(s2.length() + 1));
+        for(int i = 1; i <= x; i++) {
+            for(int j = 1; j <= y; j++) {
+                if(s1[i] == s2[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[x][y];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n,k,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(x, y, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
